4547aacfbf2419282fcb602e74227a6f7367a610
richnw <richardcawdron@hotmail.com>
2023-01-10 12:35:27 +0000

    add all rows to grid for single review

--- src/Components/SingleReview.jsx
+++ src/Components/SingleReview.jsx
@@ -1,19 +1,36 @@
 import * as api from "../api";
-import { useEffect, useState, useParams } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import moment from "moment";
 
 const SingleReview = () => {
-  const reviewID = useParams();
-  console.log(reviewID);
+  const { reviewID } = useParams();
   const [currentReview, setCurrentReview] = useState({});
 
   useEffect(() => {
-    api.fetchReview().then(({ review }) => {
+    api.fetchReview(reviewID).then(({ review }) => {
       setCurrentReview(review);
     });
-  }, []);
-  console.log(currentReview);
+  }, [reviewID]);
 
-  return currentReview.designer;
+  return (
+    <main className="SingleReview">
+      <h2>{currentReview.title}</h2>
+      <section className="SingleReviewText">
+        <p>designer: {currentReview.designer} </p>
+        <p>user: {currentReview.owner}</p>
+        {moment(currentReview.created_at).format("LLL")} <br />
+        {currentReview.review_body} <br />
+        Votes: {currentReview.votes} <br />
+        Comment Count: {currentReview.comment_count}
+      </section>
+      <img
+        className="SingleReviewImage"
+        src={`${currentReview.review_img_url}`}
+        alt={`${currentReview.title}`}
+      />
+    </main>
+  );
 };
 
 export default SingleReview;
