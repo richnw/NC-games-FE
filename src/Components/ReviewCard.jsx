9eb8d4366c787f4fa1af0fa33a0f1f14e4af0073
richnw <richardcawdron@hotmail.com>
2023-01-10 13:44:29 +0000

    add link from review card to single review page

--- src/Components/ReviewCard.jsx
+++ src/Components/ReviewCard.jsx
@@ -1,17 +1,27 @@
+import { Link } from "react-router-dom";
+
 const ReviewCard = ({ review }) => {
+  // const handleClick = (e) => {
+  //   console.log(e.target.value);
+  // };
+
   return (
     <li className="reviewCard">
-      {review.title}
-      <br />
-      <br />
-      user: {review.owner}
-      <br />
-      votes: {review.votes}
-      <img
-        className="reviewImage"
-        src={`${review.review_img_url}`}
-        alt={`${review.title}`}
-      />
+      <Link to={`/reviews/${review.review_id}`}>
+        <button>
+          {review.title}
+          <br />
+          <br />
+          user: {review.owner}
+          <br />
+          votes: {review.votes}
+          <img
+            className="reviewImage"
+            src={`${review.review_img_url}`}
+            alt={`${review.title}`}
+          />
+        </button>
+      </Link>
     </li>
   );
 };
