efffa2ddf882d2cc7e3d28d6c7046672c2acbce0
richnw <richardcawdron@hotmail.com>
2023-01-10 09:33:20 +0000

    add isLoading

--- src/Components/Reviews.jsx
+++ src/Components/Reviews.jsx
@@ -4,11 +4,16 @@ import ReviewCard from "./ReviewCard";
 
 const Reviews = () => {
   const [reviews, setReviews] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     api.fetchReviews().then(({ reviews }) => {
       setReviews(reviews);
+      setIsLoading(false);
     });
   }, []);
+
+  if (isLoading) return <h1>Loading...</h1>;
   return (
     <ul>
       {reviews.map((review) => (
