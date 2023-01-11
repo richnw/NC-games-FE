e5cfe1097ab5cd5958a4af5e0b421e0dc3f0826a
richnw <richardcawdron@hotmail.com>
2023-01-11 12:01:31 +0000

    add header link to homepage

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <header className="Header">
-      <h1>NC Games</h1>
+      <Link to={`/`}>
+        <h1>NC Games</h1>
+      </Link>
       <h2>A place for all your board game reviews</h2>
     </header>
   );
