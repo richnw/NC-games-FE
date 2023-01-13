1654eee91887251aa6c665014ce0f278808fa83b
richnw <richardcawdron@hotmail.com>
2023-01-13 14:05:19 +0000

    add sort and order functionality for reviews

--- src/Components/Reviews.jsx
+++ src/Components/Reviews.jsx
@@ -2,22 +2,31 @@ import * as api from "../api";
 import { useEffect, useState } from "react";
 import ReviewCard from "./ReviewCard";
 
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 
 import Error from "./Error";
 
 const Reviews = () => {
   const [reviews, setReviews] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [sort, setSort] = useState("votes");
+  const [order, setOrder] = useState("desc");
+  let [searchParams, setSearchParams] = useSearchParams();
 
   let { category } = useParams();
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    setSort(e.target.value);
+    setSearchParams({ sort_by: e.target.value });
+  }
+
   useEffect(() => {
-    api.fetchReviews(category).then(({ reviews }) => {
+    api.fetchReviews(sort, order, category).then(({ reviews }) => {
       setReviews(reviews);
       setIsLoading(false);
     });
-  }, [category]);
+  }, [category, sort, order]);
 
   const [error, setError] = useState(null);
 
@@ -41,7 +50,17 @@ const Reviews = () => {
 
   return (
     <main>
-      {/* <h2>{category.charAt(0).toUpperCase() + category.slice(1)}</h2> */}
+      <section>
+        <select value={sort} onChange={handleSubmit}>
+          <option value="votes">Votes</option>
+          <option value="comment_count">Comments</option>
+          <option value="created_at">Date</option>
+        </select>
+        <select value={order} onChange={(e) => setOrder(e.target.value)}>
+          <option value="desc">Descending</option>
+          <option value="asc">Ascending</option>
+        </select>
+      </section>
       <ul>
         {reviews.map((review) => (
           <ReviewCard review={review} />
